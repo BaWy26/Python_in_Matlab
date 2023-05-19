37cee21c33021e82b65733ec2cb21b928626789a
Bart Wymeersch <bart.wymeersch@external.atlascopco.com>
2023-05-19 17:00:19 +0200

    Add files

--- Python/dataframe_converter.py
+++ Python/dataframe_converter.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def convert_to_dataframe(data, columns):
     df = pd.DataFrame(data, columns=columns)
-    return df.to_dict('list')  # Convert the DataFrame to a dictionary
+    return df
